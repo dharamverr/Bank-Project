f2207dfe545fa2c422a3c97a251916a8ab90f1a9
Dharamveer Kumar <dharamveerkumar1111@gmail.com>
2025-05-10 00:32:53 +0530

    main menu created

--- Bank-Project/Program.cs
+++ Bank-Project/Program.cs
@@ -15,7 +15,7 @@ namespace Bank_Project
             System.Console.WriteLine("::Login Page::");
 
             //declare variable to store userName and Password
-            string userName=null, password=null;
+            string userName = null, password = null;
 
             //read userName from keyword
             System.Console.Write("Enter Username:");
@@ -32,9 +32,41 @@ namespace Bank_Project
             // check username and password
             if (userName == "system" && password == "manager")
             {
-                System.Console.WriteLine("TO DO: Main menu here");
-            }
+                int mainMenuChoice = -1;
+                do
+                {
+                    System.Console.WriteLine("\n:::Main Menu:::");
+                    System.Console.WriteLine("1. Customers");
+                    System.Console.WriteLine("2. Accounts");
+                    System.Console.WriteLine("3. Funds Transfer");
+                    System.Console.WriteLine("4. Funds Transfer Statement");
+                    System.Console.WriteLine("5. Account Statement");
+                    System.Console.WriteLine("0. Exit");
 
+
+
+                    System.Console.WriteLine("Enter your choice");
+                    mainMenuChoice = int.Parse(Console.ReadLine());
+                    switch (mainMenuChoice)
+                    {
+                        case 1://Customers
+                            break;
+                        case 2: //Accounts
+                            break;
+                        case 3://Funds Transfer
+                            break;
+                        case 4://Funds Transfer Statment
+                            break;
+                        case 5://Account Statement
+                            break;
+
+
+                    }
+                } while (mainMenuChoice != 0);
+            }
+            else {
+                System.Console.WriteLine("Invalid userName or password");
+            }
             //about to exit
             System.Console.WriteLine("thank you! Visit again.");
             System.Console.ReadKey();
