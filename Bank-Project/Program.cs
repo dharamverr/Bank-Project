976ebf37fa4e9c735d4183c914140d068002607d
Dharamveer Kumar <dharamveerkumar1111@gmail.com>
2025-05-09 09:53:30 +0530

    create login page

--- Bank-Project/Program.cs
+++ Bank-Project/Program.cs
@@ -8,8 +8,36 @@ namespace Bank_Project
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // Application execution start from here
         {
+            //display title
+            System.Console.WriteLine("***********************Harsha Bank*******************");
+            System.Console.WriteLine("::Login Page::");
+
+            //declare variable to store userName and Password
+            string userName=null, password=null;
+
+            //read userName from keyword
+            System.Console.Write("Enter Username:");
+            userName = Console.ReadLine();
+
+            //read password from keyboard only if username is enterd
+            if (userName != "")
+            {
+                //read password from keyword
+                System.Console.Write("Enter password:");
+                password = Console.ReadLine();
+            }
+
+            // check username and password
+            if (userName == "system" && password == "manager")
+            {
+                System.Console.WriteLine("TO DO: Main menu here");
+            }
+
+            //about to exit
+            System.Console.WriteLine("thank you! Visit again.");
+            System.Console.ReadKey();
         }
     }
 }
